424780924373b5d24675d1283d4a2d852f056152
nasir12736 <tiktokonly3331@gmail.com>
2025-12-04 07:33:48 +0500

    Create control2.js

---
+++ control2.js
@@ -0,0 +1,20 @@
+{
+  "active": true,
+  "password_required": true,
+  "announcement": {
+    "title": "⚡ Server Update",
+    "text": "Trading session starts in 10 minutes! Prepare your accounts.",
+    "type": "info",
+    "autoHide": 8000
+  },
+  "users": {
+    "uid_89283721": {
+      "name": "uusu",
+      "password": "isj"
+    },
+    "uid_992929": {
+      "name": "88",
+      "password": "@+"
+    }
+  }
+}
